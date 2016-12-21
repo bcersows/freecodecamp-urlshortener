52daf7d45a45b3a89cf1296cb1ec48afdea8229b
Bjoern <dev_mails@t-online.de>
2016-12-21 13:08:57 +0000

    Updated server port to possibly use CLoud9 given one.

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ var app = express();
 app.set('view engine', 'pug');
 app.use(require('./service_shorturl.js'));
 
-app.listen(8080, function () {
+app.listen(process.env.PORT || 8080, function () {
     console.log('Example app listening on port 8080!')
 })
